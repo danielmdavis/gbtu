e0a93e286bf4bd89ee0e937c5ab8a00a6300a928
danielmdavis <>
2021-03-30 18:59:32 -0400

    tinkering with form

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -18,11 +18,23 @@ function Signup() {
       <br/><br/>
       <br/><br/>
 
-      <h2 className='alt-subtitle'>GBTU Principles of Unity</h2>
+      <h2 className='alt-subtitle'>Organize with Us</h2>
       <br/><br/>
       <br/><br/>
       <br/><br/>
 
+      <form className='signup-form'><br/>
+        Sign up to start organizing with GBTU<br/><br/>
+        Tell us about your situation so we can build tenant power in Boston together! Fill out this form if you are a tenant who wants to join the union or start organizing your building. Your responses will be kept secure and will not be shared.<br/><br/>
+        Note: This form will not connect you directly with legal services; to receive legal support, contact Greater Boston Legal Services. </form>
+      <script className='signup-form' src="https://docs.google.com/forms/d/e/1FAIpQLSfFECJTasiU5YIgzbK3nRq2GG0qhAyhndV1CXvRKLY0N30wAw/viewform?embedded=true"
+        form="https://docs.google.com/forms/..../viewform?embedded=true">
+        <form className='signup-form' ></form>
+      </script>
+
+
+
+      <br/><br/>
       <UnityItem
         title='Our primary goal is to support the development of tenant-led unions that can be a militant organized force agitating for their own material needs.'
         body='Though we engage in coalition work, our organizing must be independent of NGOs, foundations, and government-funded groups in order to build power that is responsive to and led by tenants. We are committed to base building, taking leadership from the poorest, and resisting the power of real estate capital that destroys our homes and our communities.'
