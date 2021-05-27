2123e1dd429a2f8056bfa5aa443d3da22f803d6c
danielmdavis <>
2021-05-27 17:42:48 -0400

    all forms embedded and good

--- src/Dues.jsx
+++ src/Dues.jsx
@@ -1,11 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useScript } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import TopBar from './TopBar.jsx';
 import Footer from './Footer.jsx';
 import UnityItem from './UnityItem.jsx';
 import Logo from './images/GBTU_LOGO_600.png';
 
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
 function Dues() {
+
+
+  const createSubscription = (data, actions) => {
+    return actions.subscription.create({
+          'plan_id': 'P-01E38965615597146L6NV4WQ'
+        });
+  };
+  const createReducedSubscription = (data, actions) => {
+    return actions.subscription.create({
+          'plan_id': 'P-1UF60462HL997513HL6NVY7A'
+        });
+  };
+
+  const onApprove = (data, actions) => {
+    alert('Subscription successful!');
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -19,16 +38,32 @@ function Dues() {
       <br/><br/>
       <br/><br/>
 
-
-
-
-
       <UnityItem
         title='Joining and paying dues'
-        body='Member dues are $5 per month. Sign up for a monthly payment:'
+        body='Dues for full membership are $5 per month. Sign up for a monthly payment:'
          />
 
-  
+       <PayPalButton
+        style={{ zIndex: '0' }}
+        createSubscription={(data, actions) => createSubscription(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+
+      <br/><br/>
+      <br/><br/>
+
+      <UnityItem
+        title='Reduced dues'
+        body='If you are unemployed you may sign up for just $1 per month. Sign up for a monthly payment:'
+        />
+
+       <PayPalButton
+        style={{ zIndex: '0' }}
+        createReducedSubscription={(data, actions) => createReducedSubscription(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+
+
       <br/><br/>
       <img className="bar-logo" src={Logo}/>
       <br/><br/>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -28,7 +28,8 @@ function Signup() {
         body='Tell us about your situation so we can build tenant power in Boston together! Fill out this form if you are a tenant who wants to join the union or start organizing your building. Your responses will be kept secure and will not be shared. Note that this form will not connect you directly with legal services; to receive legal support, contact Greater Boston Legal Services.'
          />
 
-     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVpzL1tU1Uqp1GEhzluY8micf_-QM7p-hTPF4YYGVG_94rlA/viewform" width="640" height="454" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdVpzL1tU1Uqp1GEhzluY8micf_-QM7p-hTPF4YYGVG_94rlA/viewform?embedded=true" width="640" height="2274" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading…</iframe>
+
 
       <br/><br/>
       <img className="bar-logo" src={Logo}/>
